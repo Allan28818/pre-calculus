ee336911a0abbeed43e347bf6a3a073085bf4bc3
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2026-01-09 10:47:00 -0300

    feat(encryptioon-and-decryption): encryption and decryption functions were created according to composite function principles in math

---
+++ composition-encryptor/index.ts
@@ -0,0 +1,55 @@
+function encryptAsciiArray(asciiArray: number[]): number[] {
+  const encryptedArray: number[] = [];
+
+  for (let i = 0; i < asciiArray.length; i++) {
+    const asciiCode = asciiArray[i] as number;
+
+    encryptedArray.push(2 * asciiCode + 50);
+  }
+
+  return encryptedArray;
+}
+
+function decryptAsciiArray(asciiArray: number[]): number[] {
+  const decryptedAsciiArray = [];
+
+  for (let i = 0; i < asciiArray.length; i++) {
+    const asciiCode = asciiArray[i] as number;
+
+    decryptedAsciiArray.push((asciiCode - 50) / 2);
+  }
+
+  return decryptedAsciiArray;
+}
+
+function fromStringToAsciiArray(string: string): number[] {
+  const asciiArray: number[] = [];
+
+  for (let i = 0; i < string.length; i++) {
+    asciiArray.push(string.charCodeAt(i));
+  }
+
+  return asciiArray;
+}
+
+function fromAsciiArrayToString(charCodeArray: number[]): string {
+  let finalString = "";
+
+  for (let i = 0; i < charCodeArray.length; i++) {
+    const charCode = charCodeArray[i] as number;
+
+    finalString += String.fromCharCode(charCode);
+  }
+
+  return finalString;
+}
+
+const myString = "Ola Mundo!";
+const asciiArray = fromStringToAsciiArray(myString);
+const encryptedArray = encryptAsciiArray(asciiArray);
+const decryptedArray = decryptAsciiArray(encryptedArray);
+
+console.log(asciiArray, fromAsciiArrayToString(asciiArray));
+console.log(encryptedArray, fromAsciiArrayToString(encryptedArray));
+
+console.log(decryptedArray, fromAsciiArrayToString(decryptedArray));
